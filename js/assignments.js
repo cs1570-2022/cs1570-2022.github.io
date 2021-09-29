dcf15cfc24759646756720800018e1aad29f1ae3
dai-dai-dai <anna_dai@brown.edu>
2021-09-28 20:13:07 -0400

    sol 1

--- js/assignments.js
+++ js/assignments.js
@@ -158,7 +158,7 @@ Vue.component('page-content', {
                     due: '09/23 2:30pm',
                     latexTemplate: 'HW0 LaTeX',
                     latexTemplateURL: '../content/homeworks/hw0-2021.tex',
-                    solutionURL: '../content/homeworks/hw1-2021-sol.pdf',
+                    solutionURL: '',
                 },
                 {
                     name: 'Homework 1',
@@ -167,7 +167,7 @@ Vue.component('page-content', {
                     due: '09/21 2:30pm',
                     latexTemplate: 'HW1 LaTeX',
                     latexTemplateURL: '../content/homeworks/hw1-2021.tex',
-                    solutionURL: '',
+                    solutionURL: '../content/homeworks/hw1-2021-sol.pdf',
                 },
                 {
                     name: 'Homework 2',
