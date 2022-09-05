e4f73f051aa888776d6377765f8e2092724063c6
Robert Scheidegger <robert_scheidegger@brown.edu>
2022-09-05 14:49:46 -0400

    refactor, add TeX clarification

--- src/sections/Assignments.jsx
+++ src/sections/Assignments.jsx
@@ -4,89 +4,7 @@ import { PageCountdownTime } from "../components/PageCountdownTime";
 import { PageSectionTitle } from "../components/PageSectionTitle";
 import { PageTable } from "../components/PageTable";
 import { Colors } from "../Constants";
-
-const AssignmentsList = [
-    {
-        name: 'Homework 0',
-        handoutURL: '../content/homeworks/hw0.pdf',
-        out: '09/08 12:00 pm',
-        due: '09/13 2:30 pm',
-        latexTemplateURL: '',
-        solutionURL: '',
-    },
-    {
-        name: 'Homework 1',
-        handoutURL: '../content/homeworks/hw1.pdf',
-        out: '09/13 3:50 pm',
-        due: '09/20 2:30 pm',
-        latexTemplateURL: '../content/homeworks/hw1.tex',
-        solutionURL: '../content/homeworks/hw1-sol.pdf',
-    },
-    {
-        name: 'Homework 2',
-        handoutURL: '../content/homeworks/hw2.pdf',
-        out: '09/20 3:50 pm',
-        due: '09/27 2:30 pm',
-        latexTemplateURL: '../content/homeworks/hw2.tex',
-        solutionURL: '../content/homeworks/hw2-sol.pdf',
-    },
-    {
-        name: 'Homework 3',
-        handoutURL: '../content/homeworks/hw3.pdf',
-        out: '09/27 3:50 pm',
-        due: '10/04 2:30 pm',
-        latexTemplateURL: '../content/homeworks/hw3.tex',
-        solutionURL: '../content/homeworks/hw3-sol.pdf',
-    },
-    {
-        name: 'Homework 4',
-        handoutURL: '../content/homeworks/hw4.pdf',
-        out: '10/04 3:50 pm',
-        due: '10/11 2:30 pm',
-        latexTemplateURL: '../content/homeworks/hw4.tex',
-        solutionURL: '../content/homeworks/hw4-sol.pdf',
-    },
-    {
-        name: 'Homework 5',
-        handoutURL: '../content/homeworks/hw5.pdf',
-        out: '10/11 3:50 pm',
-        due: '10/18 2:30 pm',
-        latexTemplateURL: '../content/homeworks/hw5.tex',
-        solutionURL: '../content/homeworks/hw5-sol.pdf',
-    },
-    {
-        name: 'Homework 6',
-        handoutURL: '../content/homeworks/hw6.pdf',
-        out: '10/25 3:50 pm',
-        due: '11/01 2:30 pm',
-        latexTemplateURL: '../content/homeworks/hw6.tex',
-        solutionURL: '../content/homeworks/hw6-sol.pdf',
-    },
-    {
-        name: 'Homework 7',
-        handoutURL: '../content/homeworks/hw7.pdf',
-        out: '11/1 3:50 pm',
-        due: '11/10 2:30 pm',
-        latexTemplateURL: '../content/homeworks/hw7.tex',
-        solutionURL: '../content/homeworks/hw7-sol.pdf',
-    },
-    {
-        name: 'Homework 8',
-        handoutURL: '../content/homeworks/hw8.pdf',
-        out: '11/10 3:50 pm',
-        due: '11/17 2:30 pm',
-        latexTemplateURL: '../content/homeworks/hw8.tex',
-        solutionURL: '../content/homeworks/hw8-sol.pdf',
-    },
-    {
-        name: 'Homework 9',
-        handoutURL: '../content/homeworks/hw9.pdf',
-        out: '11/17 3:50 pm',
-        due: '12/01 2:30 pm',
-        latexTemplateURL: '../content/homeworks/hw9.tex',
-        solutionURL: '../content/homeworks/hw9-sol.pdf',
-    },
-];
+import { AssignmentsList } from "../AssignmentList";
 
 export const CourseAssignment = (props) => {
     const { handoutURL, name, out, due, latexTemplateURL, solutionURL } = props;
@@ -120,6 +38,9 @@ export const Assignments = () => {
             >
                 <p>All assignments must be typeset using LateX and submitted on <a href='https://www.gradescope.com'>Gradescope</a>.</p>
                 <p>Please refer to the syllabus for the collaboration policy and late submission policy of the class.</p>
+                <p>
+                    <b>LaTeX Note:</b> If you want to use the provided LaTeX templates for the assignments, you will need to download <mark>cs.cls</mark> and <mark>cs157.cls</mark> from the <a>Resources</a> section below, and place them in your LaTeX environment in a folder called <mark>common</mark>.
+                </p>
                 <PageTable
                     tableheads={["Assignment #", "Out", "Due", "LaTeX", "Solution"]}
                 >
